c972ddbd8841533c0ac98674c8aff91f5c52ac3f
Enow Divine <sirdivine16@gmail.com>
2022-08-01 19:50:15 +0100

    added react icons

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Work.css";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { BiRightArrow } from "react-icons/bi";
 
 const Work = () => {
   return (
@@ -9,7 +10,7 @@ const Work = () => {
         <h4>Where I've Worked</h4>
       </div>
       <div className="div-tabs">
-        <Tabs className="my-tabs">
+        <Tabs className="my-tabs text-left">
           <TabList>
             <Tab>
               <p>Title 1</p>
@@ -29,9 +30,24 @@ const Work = () => {
           </TabList>
 
           <div>
-            <TabPanel>
+            <TabPanel className="work-details">
               <div className="panel-content">
-                <h2>Any content 1</h2>
+                <p className="work-place">
+                  Full Stack Developer{" "}
+                  <span className="company">
+                    @ <a href="https://summittech-eng.org/">Summit Tech</a>
+                  </span>
+                </p>
+                <span className="work-period">July - Dec 2021</span>
+                <div>
+                  <ol>
+                    <li>
+                      <BiRightArrow />
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Voluptatem, fugit ullam voluptas officiis quidem delectus
+                    </li>
+                  </ol>
+                </div>
               </div>
             </TabPanel>
             <TabPanel>
